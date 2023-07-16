8eafd274a6ead2ec6acfc6126927d1c223f2d3c0
Shubham Dixit <shubhamrajneshdixit@gmail.com>
2023-07-16 18:57:22 +0530

    Rect Bootstrap

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Header } from "./components/header";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
